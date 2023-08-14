bad297e7fa18cbadd7ba8b8efb521d599753ecb2
Charles Tobler <cjtobler33@yahoo.com>
2023-08-14 09:29:05 -0500

    accidentally merged first pull

--- week-01/assessment/CapsuleHotel.java
+++ week-01/assessment/CapsuleHotel.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class CapsuleHotel
 {
+    // Dev10 Week01 Assessment
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
